ffbab2132692c722964240c67507ff49021792aa
Art <WildandArt@users.noreply.github.com>
2022-12-06 06:01:15 +0200

    Initial Commit

---
+++ Stocks App Replica with MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks App Replica with MVP
+//
+//  Created by Artemy Ozerski on 06/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
